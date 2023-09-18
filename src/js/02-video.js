763a08195d37840c61c106568cb513130408ce39
wielembe <e.wielemborek@gmail.com>
2023-09-18 17:12:26 +0200

    Update 02-video.js
    testing (as error occured)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,36 +1,36 @@
-import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+// import Player from '@vimeo/player';
+// //import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
-const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+// const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
 
-const timeUpdate = throttle(data => {
-  localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
-}, 1000);
+// const timeUpdate = throttle(data => {
+//   localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
+// }, 1000);
 
-player.on('timeupdate', timeUpdate);
+// player.on('timeupdate', timeUpdate);
 
-player.on('loaded', () => {
-  const savedTimeUpdate = localStorage.getItem(LOCALSTORAGE_KEY);
-  if (savedTimeUpdate) {
-    player
-      .setCurrentTime(Number.parseFloat(savedTimeUpdate))
-      .catch(function (error) {
-        switch (error.name) {
-          case 'RangeError':
-            break;
-          default:
-            break;
-        }
-      });
-  }
-});
+// player.on('loaded', () => {
+//   const savedTimeUpdate = localStorage.getItem(LOCALSTORAGE_KEY);
+//   if (savedTimeUpdate) {
+//     player
+//       .setCurrentTime(Number.parseFloat(savedTimeUpdate))
+//       .catch(function (error) {
+//         switch (error.name) {
+//           case 'RangeError':
+//             break;
+//           default:
+//             break;
+//         }
+//       });
+//   }
+// });

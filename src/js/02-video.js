73364aa094ace223c301db5874eac49e5899a440
wielembe <e.wielemborek@gmail.com>
2023-09-18 17:38:10 +0200

    testing load
    test

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,36 +1,37 @@
-// import Player from '@vimeo/player';
-// //import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
-// const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+import throttle from 'lodash.throttle';
 
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
-// player.getVideoTitle().then(function (title) {
-//   console.log('title:', title);
-// });
+player.on('play', function () {
+  console.log('played the video!');
+});
 
-// const timeUpdate = throttle(data => {
-//   localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
-// }, 1000);
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
 
-// player.on('timeupdate', timeUpdate);
+const timeUpdate = throttle(data => {
+  localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
+}, 1000);
 
-// player.on('loaded', () => {
-//   const savedTimeUpdate = localStorage.getItem(LOCALSTORAGE_KEY);
-//   if (savedTimeUpdate) {
-//     player
-//       .setCurrentTime(Number.parseFloat(savedTimeUpdate))
-//       .catch(function (error) {
-//         switch (error.name) {
-//           case 'RangeError':
-//             break;
-//           default:
-//             break;
-//         }
-//       });
-//   }
-// });
+player.on('timeupdate', timeUpdate);
+
+player.on('loaded', () => {
+  const savedTimeUpdate = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (savedTimeUpdate) {
+    player
+      .setCurrentTime(Number.parseFloat(savedTimeUpdate))
+      .catch(function (error) {
+        switch (error.name) {
+          case 'RangeError':
+            break;
+          default:
+            break;
+        }
+      });
+  }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+let formState = {
+  email: '',
+  message: '',
+};
+
+const checkStorage = localStorage.getItem(LOCALSTORAGE_KEY);
+if (checkStorage) {
+  formState = JSON.parse(checkStorage);
+  form.elements.email.value = formState.email;
+  form.elements.message.value = formState.message;
+}
+
+const throttledSave = throttle(data => {
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(data));
+}, 500);
+
+function saveData(evt) {
+  formState[evt.target.name] = evt.target.value;
+  throttledSave(formState);
+}
+form.addEventListener('input', saveData);
+
+const sentForm = eventSent => {
+  eventSent.preventDefault();
+  const {
+    elements: { email, message },
+  } = eventSent.currentTarget;
+  const objectData = {
+    email: email.value,
+    message: message.value,
+  };
+  console.log(objectData);
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  formState = {
+    email: '',
+    message: '',
+  };
+  form.reset();
+};
+form.addEventListener('submit', sentForm);

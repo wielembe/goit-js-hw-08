b714360c4522959fd9d99c853dfff9509f4c12b8
wielembe <e.wielemborek@gmail.com>
2023-09-18 16:49:23 +0200

    Update 01-gallery.js
    task 1 based on previous homework

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -10,37 +10,24 @@ const galleryList = document.querySelector('.gallery');
 
 const galleryElements = galleryItems
   .map(picture => {
-    return `<li class="gallery__item">
-     <a class="gallery__link" href="${picture.original}">
+    return `<a class="gallery__item" href="${picture.original}">
        <img
         class="gallery__image"
          src="${picture.preview}"
-         data-source="${picture.original}"
+
          alt="${picture.description}"
        />
      </a>
-   </li>`;
+   `;
   })
   .join('');
 
 galleryList.innerHTML = galleryElements;
 
-galleryList.addEventListener('click', selectImage);
-
-function selectImage(event) {
-  event.preventDefault();
-  if (event.target.nodeName !== 'IMG') {
-    return;
-  }
-  const selectedImage = event.target.dataset.source;
-  const openedImage = basicLightbox.create(`<img src=` + selectedImage + `>`);
-  openedImage.show();
-
-  document.addEventListener('keydown', escapePress);
-  function escapePress(eventKey) {
-    if (eventKey.code === 'Escape') {
-      openedImage.close();
-      document.removeEventListener('keydown', escapePress);
-    }
-  }
-}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionSelector: 'self',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
